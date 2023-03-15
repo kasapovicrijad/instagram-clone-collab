fde291ccd20f63345db2b9e62c92dba17da3871b
bilalIam <bilal.mujic@gmail.com>
2023-03-15 12:34:49 +0100

    data folder, new path, post form fixed

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const express = require("express");
-
+const path = require('path');
+const fs = require('fs');
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', 'views');
-
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('index');
+});
+
+app.get('/post', (req, res) => {
+    res.render('post-form');
+});
+
+app.post('/post', (req, res) => {
+    const { title, picUrl, caption} = req.body;
+    const put = path.join(__dirname, "data", "posts.json");
+    fs.readFile(put, (err, posts) => {
+        const updatedPosts =[{ title, picUrl, caption}, ...JSON.parse(posts)];
+        fs.writeFile(put, JSON.stringify(updatedPosts), () => {
+            res.redirect('/');
+        })
+    })
 });
 
 app.listen(5000);
